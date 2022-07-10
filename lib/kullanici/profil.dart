39f2c49494710626a739c2b87a4800cbb2099b4c
FURKAN ÇAĞLAR <furkan214650@ogr.duzce.edu.tr>
2022-07-10 22:44:55 +0300

    changes

--- lib/kullanici/profil.dart
+++ lib/kullanici/profil.dart
@@ -9,42 +9,44 @@ class ProfilSayfasi extends StatefulWidget {
 
 class _ProfilSayfasiState extends State<ProfilSayfasi> {
   @override
-  void initState() {
-    super.initState();
-    debugPrint("initState Calisti");
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    debugPrint("didChangeDependencies Calisti");
-  }
-
-  @override
   Widget build(BuildContext context) {
-    debugPrint("Build Calisti$context");
     return Scaffold(
       appBar: AppBar(
         title: const Text("Profil Sayfası"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  debugPrint("Set State Calisti");
-                });
-              },
-              icon: const Icon(Icons.abc))
-        ],
       ),
-      body: const Center(
-        child: Text("Profil Sayfası"),
-      ),
+      body: const Center(child: Text("Profil")),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.sports_basketball,
+            color: Color.fromARGB(255, 4, 3, 91),
+            size: 30,
+          ),
+          label: "Anasayfa",
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.flag_circle_outlined,
+              color: Color.fromARGB(255, 4, 3, 91),
+              size: 30,
+            ),
+            label: "Duyurular"),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.people,
+              color: Color.fromARGB(255, 4, 3, 91),
+              size: 30,
+            ),
+            label: "Profil"),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.menu_rounded,
+            color: Color.fromARGB(255, 4, 3, 91),
+            size: 30,
+          ),
+          label: "Menu",
+        ),
+      ]),
     );
-  }
-
-  @override
-  void dispose() {
-    debugPrint("Dispose Calisti");
-    super.dispose();
   }
 }

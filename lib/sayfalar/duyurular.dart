48d88e385ae52fe26f8e98e584d847cbacde11b8
FURKAN ÇAĞLAR <furkan214650@ogr.duzce.edu.tr>
2022-07-12 00:07:41 +0300

    changes

--- lib/sayfalar/duyurular.dart
+++ lib/sayfalar/duyurular.dart
@@ -12,6 +12,8 @@ class _DuyurularState extends State<Duyurular> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        automaticallyImplyLeading: false,
         title: const Text(
           "Anadolu Efes Fan Club  \n         Kampanyaları",
           style: TextStyle(
@@ -27,9 +29,12 @@ class _DuyurularState extends State<Duyurular> {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    Container(
-                      child: Center(
-                        child: Image.asset("assets/resimler/konix.png"),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Container(
+                        child: Center(
+                          child: Image.asset("assets/resimler/konix.png"),
+                        ),
                       ),
                     ),
                     TextButton(
@@ -44,23 +49,297 @@ class _DuyurularState extends State<Duyurular> {
                     Container(
                       width: double.infinity,
                       child: ElevatedButton.icon(
-                          onPressed: () {
-                            Navigator.pushNamed(context, "/konix");
-                          },
-                          icon: const Icon(
-                            Icons.arrow_right_alt,
-                            size: 35,
-                            color: Color.fromARGB(255, 27, 126, 172),
-                          ),
-                          label: const Text(
-                            "Kampanya Detayı",
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 27, 126, 172)),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Color.fromARGB(255, 244, 250, 255),
-                            shadowColor: Color.fromARGB(255, 244, 250, 255),
-                          )),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/konix");
+                        },
+                        icon: const Icon(
+                          Icons.arrow_right_alt,
+                          size: 35,
+                          color: Color.fromARGB(255, 27, 126, 172),
+                        ),
+                        label: const Text(
+                          "Kampanya Detayı",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 126, 172)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 244, 250, 255),
+                          shadowColor: Color.fromARGB(255, 244, 250, 255),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Container(
+                        child: Center(
+                          child: Image.asset("assets/resimler/konix.png"),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/konix");
+                      },
+                      child: const Text(
+                        "Konix'ten Fan Club'lılara %25 İndirim!",
+                        style: TextStyle(color: Colors.blue, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/konix");
+                        },
+                        icon: const Icon(
+                          Icons.arrow_right_alt,
+                          size: 35,
+                          color: Color.fromARGB(255, 27, 126, 172),
+                        ),
+                        label: const Text(
+                          "Kampanya Detayı",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 126, 172)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 244, 250, 255),
+                          shadowColor: Color.fromARGB(255, 244, 250, 255),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Container(
+                        child: Center(
+                          child: Image.asset("assets/resimler/konix.png"),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/konix");
+                      },
+                      child: const Text(
+                        "Konix'ten Fan Club'lılara %25 İndirim!",
+                        style: TextStyle(color: Colors.blue, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/konix");
+                        },
+                        icon: const Icon(
+                          Icons.arrow_right_alt,
+                          size: 35,
+                          color: Color.fromARGB(255, 27, 126, 172),
+                        ),
+                        label: const Text(
+                          "Kampanya Detayı",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 126, 172)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 244, 250, 255),
+                          shadowColor: Color.fromARGB(255, 244, 250, 255),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Container(
+                        child: Center(
+                          child: Image.asset("assets/resimler/konix.png"),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/konix");
+                      },
+                      child: const Text(
+                        "Konix'ten Fan Club'lılara %25 İndirim!",
+                        style: TextStyle(color: Colors.blue, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/konix");
+                        },
+                        icon: const Icon(
+                          Icons.arrow_right_alt,
+                          size: 35,
+                          color: Color.fromARGB(255, 27, 126, 172),
+                        ),
+                        label: const Text(
+                          "Kampanya Detayı",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 126, 172)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 244, 250, 255),
+                          shadowColor: Color.fromARGB(255, 244, 250, 255),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Container(
+                        child: Center(
+                          child: Image.asset("assets/resimler/konix.png"),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/konix");
+                      },
+                      child: const Text(
+                        "Konix'ten Fan Club'lılara %25 İndirim!",
+                        style: TextStyle(color: Colors.blue, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/konix");
+                        },
+                        icon: const Icon(
+                          Icons.arrow_right_alt,
+                          size: 35,
+                          color: Color.fromARGB(255, 27, 126, 172),
+                        ),
+                        label: const Text(
+                          "Kampanya Detayı",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 126, 172)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 244, 250, 255),
+                          shadowColor: Color.fromARGB(255, 244, 250, 255),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Container(
+                        child: Center(
+                          child: Image.asset("assets/resimler/konix.png"),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/konix");
+                      },
+                      child: const Text(
+                        "Konix'ten Fan Club'lılara %25 İndirim!",
+                        style: TextStyle(color: Colors.blue, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/konix");
+                        },
+                        icon: const Icon(
+                          Icons.arrow_right_alt,
+                          size: 35,
+                          color: Color.fromARGB(255, 27, 126, 172),
+                        ),
+                        label: const Text(
+                          "Kampanya Detayı",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 126, 172)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 244, 250, 255),
+                          shadowColor: Color.fromARGB(255, 244, 250, 255),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Container(
+                        child: Center(
+                          child: Image.asset("assets/resimler/konix.png"),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/konix");
+                      },
+                      child: const Text(
+                        "Konix'ten Fan Club'lılara %25 İndirim!",
+                        style: TextStyle(color: Colors.blue, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/konix");
+                        },
+                        icon: const Icon(
+                          Icons.arrow_right_alt,
+                          size: 35,
+                          color: Color.fromARGB(255, 27, 126, 172),
+                        ),
+                        label: const Text(
+                          "Kampanya Detayı",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 126, 172)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 244, 250, 255),
+                          shadowColor: Color.fromARGB(255, 244, 250, 255),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Container(
+                        child: Center(
+                          child: Image.asset("assets/resimler/konix.png"),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/konix");
+                      },
+                      child: const Text(
+                        "Konix'ten Fan Club'lılara %25 İndirim!",
+                        style: TextStyle(color: Colors.blue, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/konix");
+                        },
+                        icon: const Icon(
+                          Icons.arrow_right_alt,
+                          size: 35,
+                          color: Color.fromARGB(255, 27, 126, 172),
+                        ),
+                        label: const Text(
+                          "Kampanya Detayı",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 126, 172)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 244, 250, 255),
+                          shadowColor: Color.fromARGB(255, 244, 250, 255),
+                        ),
+                      ),
                     ),
                   ],
                 ),
